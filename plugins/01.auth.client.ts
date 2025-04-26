fcc2fc7b34dfae9d3ef528654d606284c7b1cfff
Lyu Ji <hkfhfkcoc@protonmail.com>
2025-04-26 16:49:40 -0700

    hmmm....

---
+++ plugins/01.auth.client.ts
@@ -0,0 +1,21 @@
+export default defineNuxtPlugin({
+	name: "better-auth-fetch-plugin-client",
+	async setup(nuxtApp) {
+		if (!nuxtApp.payload.serverRendered) {
+			await useAuth().fetchSession();
+		} else if (
+			Boolean(nuxtApp.payload.prerenderedAt) ||
+			Boolean(nuxtApp.payload.isCached)
+		) {
+			nuxtApp.hook("app:mounted", async () => {
+				await useAuth().fetchSession();
+			});
+		} else if (import.meta.client) {
+			const userSession = await useAuth().fetchSession();
+			if (!userSession?.session.token) {
+				const { signIn } = useAuth();
+				await signIn.anonymous();
+			}
+		}
+	},
+});
