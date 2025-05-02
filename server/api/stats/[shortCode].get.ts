c8942e204570557285a99b05fa4b90b331d1f9c0
Lyu Ji <hkfhfkcoc@protonmail.com>
2025-05-01 17:40:27 -0700

    hmmm

--- server/api/stats/[shortCode].get.ts
+++ server/api/stats/[shortCode].get.ts
@@ -1,28 +1,29 @@
 import { useDrizzle } from "../../utils/drizzle";
 
-export default defineEventHandler(async (event) => {
-	const shortCode = getRouterParam(event, "shortCode");
+export default defineEventHandler({
+	onRequest: [requireAuth],
+	handler: async (event) => {
+		const shortCode = getRouterParam(event, "shortCode");
 
-	if (!shortCode) {
-		throw createError({
-			statusCode: 422,
-			message: "Short code is required",
+		if (!shortCode) {
+			throw createError({
+				statusCode: 422,
+				message: "Short code is required",
+			});
+		}
+
+		const db = useDrizzle();
+		const url = await db.query.url.findFirst({
+			where: (url, { eq }) => eq(url.shortCode, shortCode),
 		});
-	}
 
-	const db = useDrizzle();
+		if (!url) {
+			throw createError({
+				statusCode: 404,
+				message: "URL not found",
+			});
+		}
 
-	// Look up the URL in the database
-	const url = await db.query.url.findFirst({
-		where: (url, { eq }) => eq(url.shortCode, shortCode),
-	});
-
-	if (!url) {
-		throw createError({
-			statusCode: 404,
-			message: "URL not found",
-		});
-	}
-
-	return url;
+		return url;
+	},
 });
